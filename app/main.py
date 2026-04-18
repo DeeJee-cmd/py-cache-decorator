38efee13bfcee6e67b5d0e162c822af2bc6a768e
Pasha Liashenko <zedxi236@ukr.net>
2026-04-19 00:35:24 +0300

    Add cache decorator

--- app/main.py
+++ app/main.py
@@ -2,5 +2,16 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    def wrapper(*args, **kwargs):
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_store:
+            print("Getting from cache")
+        else:
+            print("Calculating new result")
+            cache_store[key] = func(*args, **kwargs)
+
+        return cache_store[key]
+
+    return wrapper
